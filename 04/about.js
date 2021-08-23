fde1d42a89ab83f5916d8240c88ad666b698c2df
xuezhonglin <xzl0318@126.com>
2021-08-23 18:16:57 +0800

    企业站

--- 04/about.js
+++ 04/about.js
@@ -2,11 +2,37 @@
 // 创建Map实例
 let map = new BMap.Map("map");
 // 初始化地图,设置中心点坐标和地图级别
-map.centerAndZoom(new BMap.Point(104.078144,30.545828), 18);
+let point = new BMap.Point(104.078144,30.545828);
+map.centerAndZoom(point, 18);
 // 开启鼠标滚轮缩放
 map.enableScrollWheelZoom(true);
 // 创建Marker标注
-let pt = new BMap.Point(104.078144,30.545828);
-let marker = new BMap.Marker(pt);
+let marker = new BMap.Marker(point);
 // 将标注添加到地图
 map.addOverlay(marker);
+
+// 添加地图控件
+map.addControl(new BMap.NavigationControl());    
+map.addControl(new BMap.ScaleControl());    
+map.addControl(new BMap.OverviewMapControl());   
+map.addControl(new BMap.GeolocationControl());   
+
+var geolocation = new BMap.Geolocation();
+// 开启SDK辅助定位
+geolocation.enableSDKLocation();
+geolocation.getCurrentPosition(function(r){
+    r.point = point;
+    r.latitude = point.lat;
+    r.longitude = point.lng;
+    r = {...r};
+	if(this.getStatus() == BMAP_STATUS_SUCCESS){
+		map.panTo(point);
+        let ads = "";
+        const {province,city,street,street_number} = r.address;
+        ads += province + city + street + street_number;
+        $(".ads").text(ads);
+	}
+	else {
+		alert('failed'+this.getStatus());
+	}        
+});
--- 04/news-detail.js
+++ 04/news-detail.js
@@ -4,32 +4,8 @@ function getParams(){
     let id = str.charAt(str.length-1);
     return id;
 }
-// 发送请求
-// axios({
-//     url:"//mock-api.com/rg1Mk4gY.mock/details",
-//     method:"get"
-// })
-// .then(res=>{    
-//     let detail = res.data.filter(item=>item.id==getParams())[0];
-//     let w = $(".content img").width();
-//     let u = detail.url.split(",");
-//     $(".title p").text(detail.title);
-//     $(".title span").text(detail.date);
-//     if(w>=318){
-//         $(".content img").attr("src",u[2]);
-//     }else if(w>=180){
-//         $(".content img").attr("src",u[1]);
-//     }else {
-//         $(".content img").attr("src",u[0]);
-//     }
-//     $(".content img").attr("alt",detail.text);
-//     $(".info").text(detail.info);
-//     $(".indc").text(detail.indc);
-// })
 
-let reg = /[\w | \s | \< | \> | \/ | (!--#--="") | ▼ | 《》]/gi;
 let details = JSON.parse(sessionStorage.getItem("details"));
-console.log("details",details);
 // 标题
 $(".title p").text(details.title);
 $(".title span").text(details.time);
@@ -46,4 +22,4 @@ if(w>=1024){
     $(".content img").attr("src",u[2].imgSrc);
 }
 // 内容
-$(".info").text(details.content.replace(reg,""));
+$(".info").html(details.content);
