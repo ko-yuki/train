3ce99a6219633e25bbb49de104cab27e3d551dce
xuezhonglin <xzl0318@126.com>
2021-08-27 14:45:18 +0800

    删除06和08

--- 06/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import Wellborn from "@/pages/Wellborn";
-
-ReactDOM.render(<Wellborn />, document.getElementById("app"));
